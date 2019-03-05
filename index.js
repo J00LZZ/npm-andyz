4eba1a611c210c54fb09610c985263f1d0fc43c2
Victor Roest <victor@xirion.net>
2019-03-05 19:27:18 +0100

    Removed totally unessecary dependency

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+const { readFileSync } = require('fs')
+
 console.log("Knock knock, Andy is here to test your tests!\n...")
-const loadJsonFile = require('load-json-file');
 
 class ProductNotFinishedError extends Error {
     constructor(message) {
@@ -12,13 +13,13 @@ function notFinished() {
     throw new ProductNotFinishedError(`\nAndy Zaidman's voice echoed: "Testing is very important" Please specify a test file in package.json.\n`);
 }
 
-loadJsonFile('package.json').then(json => {
-    if (json.scripts.test) {
-        if (json.scripts.test == "echo \"Error: no test specified\" && exit 1") {
-            notFinished()
-        }
-        console.log("Good job, you will not be fired from this course.")
-    } else {
+const json = JSON.parse(readFileSync("package.json"))
+
+if (json.scripts.test) {
+    if (json.scripts.test == "echo \"Error: no test specified\" && exit 1") {
         notFinished()
     }
-});
+    console.log("Good job, you will not be fired from this course.")
+} else {
+    notFinished()
+}
