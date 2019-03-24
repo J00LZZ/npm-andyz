5dd5c5f09d015d4fd11c70b9b7a98fdf4af39dbf
Ricardo Vogel <info@ricardovogel.nl>
2019-03-24 17:52:07 +0100

    Add some a e s t h e t i c

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
+require("colors")
 const { readFileSync } = require('fs')
 
-console.log("Knock knock, Andy is here to test your tests!\n...")
+console.log("Knock knock, Andy is here to test your tests!\n...".rainbow)
 
 class ProductNotFinishedError extends Error {
     constructor(message) {
         super(message);
-        this.name = "ProductNotFinishedError";
+        this.name = "ProductNotFinishedError".rainbow;
     }
 }
 
 function notFinished() {
-    throw new ProductNotFinishedError(`\nAndy Zaidman's voice echoed: "Testing is very important" Please specify a test file in package.json.\n`);
+    throw new ProductNotFinishedError(`\nAndy Zaidman's voice echoed: "Testing is very important" Please specify a test file in package.json.\n`.rainbow);
 }
 
 const json = JSON.parse(readFileSync("package.json"))
@@ -19,7 +20,7 @@ if (json.scripts.test) {
     if (json.scripts.test == "echo \"Error: no test specified\" && exit 1") {
         notFinished()
     }
-    console.log("Good job, you will not be fired from this course.")
+    console.log("Good job, you will not be fired from this course.".rainbow)
 } else {
     notFinished()
 }
